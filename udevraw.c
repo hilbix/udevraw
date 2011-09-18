48dd7a827b9b78b3608dfc307fd07c80b1e43a7c
Valentin Hilbig <webmaster@scylla-charybdis.com>
2011-09-18 13:13:39 +0000

    Options -a -e -i -p

--- udevraw.c
+++ udevraw.c
@@ -6,8 +6,8 @@
  * (Note that tinolib in the tino/ subdirectory is not CLL!)
  *
  * $Log$
- * Revision 1.1  2011-04-11 22:36:07  tino
- * *** empty log message ***
+ * Revision 1.2  2011-09-18 13:13:39  tino
+ * Options -a -e -i -p
  *
  */
 
@@ -22,8 +22,12 @@
 static struct udev *udev;
 static struct udev_monitor *mon;
 static struct udev_device *dev;
-
+static struct udev_queue *queue;
+static const char *prefix, *extended;
+static int out;
 static int quiet;
+
+static int first;
 
 static void
 cleanup(void)
@@ -32,6 +36,11 @@ cleanup(void)
     {
       udev_device_unref(dev);
       dev = 0;
+    }
+  if (queue)
+    {
+      udev_queue_unref(queue);
+      queue = 0;
     }
   if (mon)
     {
@@ -92,12 +101,98 @@ nonnull(void *ptr, const char *s, ...)
   return NULL;
 }
 
+static void
+param(const char *what, const char *mid)
+{
+  if (!first)
+    tino_io_put(out, ' ');
+  first = 0;
+  if (prefix)
+    tino_put_s(out, prefix);
+  if (mid)
+    tino_put_s(out, mid);
+  tino_put_s(out, what);
+  tino_io_put(out, '=');
+}
+
+static const char *
+check(int force, const char *what)
+{
+  if (force<1)
+    {
+      if (!extended || (*extended!='*' && !strstr(extended, what)))
+        return NULL;
+    }
+  if (extended && *extended)
+    {
+      const char *tmp;
+
+      if (*extended=='!' && extended[1]=='*')
+        return NULL;
+
+      /* check for disabled with !what */
+      tmp = strstr(extended+1, what);
+      if (tmp && *--tmp=='!')
+	return NULL;
+    }
+  if (force>=0)
+    param(what, NULL);
+  return what;
+}
+
+static void
+outs(int force, const char *what, const char *fn(struct udev_device *))
+{
+  if (check(force, what))
+    tino_put_ansi_if(out, fn(dev));
+}
+
+static void
+lister(struct udev_list_entry *list)
+{
+  struct udev_list_entry *next;
+
+  next = udev_list_entry_get_next(list);
+  if (!next)
+    {
+      tino_put_ansi_if(out, udev_list_entry_get_name(list));
+      return;
+    }
+
+  tino_put_ansi_start(out);
+  tino_put_ansi(out, udev_list_entry_get_name(list), NULL);
+  do
+    {
+      tino_put_ansi_c(out, ' ', NULL);
+      tino_put_ansi(out, udev_list_entry_get_name(next), NULL);
+      next = udev_list_entry_get_next(next);
+    } while (next);
+  tino_put_ansi_end(out);
+}
+
+static void
+lister2(struct udev_device *dev, const char *tag,
+	struct udev_list_entry *fn(struct udev_device *),
+	const char *val(struct udev_device *, const char *))
+{
+  struct udev_list_entry *list;
+
+  for (list=fn(dev); list; list=udev_list_entry_get_next(list))
+    {
+      const char *name;
+
+      name = udev_list_entry_get_name(list);
+      param(name, tag);
+      tino_put_ansi_if(out, val(dev,name));
+    }
+}
+
 int
 main(int argc, char **argv)
 {
   int	argn;
-  const char *src, *subsystem, *devtype;
-  int	out;
+  const char *src, *subsystem, *devtype, *action;
+  int	idle;
 
   argn	= tino_getopt(argc, argv, 0, 0,
 		      TINO_GETOPT_VERSION(UDEVRAW_VERSION)
@@ -105,8 +200,8 @@ main(int argc, char **argv)
 		      "	Dumps UDEV events to stdout.\n"
 		      "	Unbuffered and directly usable by shells as it ought to be.\n"
 		      "	Example:\n"
-		      "	# while read -ru3 seq action subsys devtype device args; do\n"
-		      "	#   eval $args\n"
+		      "	# while read -ru3 args; do\n"
+		      "	#   eval \"$args\"\n"
 		      "	#   ...\n"
 		      "	# done 3< <(udevraw)"
 		      ,
@@ -116,12 +211,29 @@ main(int argc, char **argv)
 		      ,
 
 		      TINO_GETOPT_STRING
+		      "a actn	filter for Action (like 'change')"
+		      , &action,
+
+		      TINO_GETOPT_STRING
 		      "d dev	filter for Devtype (like 'disk')"
 		      , &devtype,
+
+		      TINO_GETOPT_STRING
+		      "e attr	Enable extended output (like 'syspath')\n"
+		      "		'!att' to suppress output, start with '*' for all"
+		      , &extended,
 		      
 		      TINO_GETOPT_STRING
 		      "f sys	Filter for subsystem (like 'block')"
 		      , &subsystem,
+
+		      TINO_GETOPT_FLAG
+		      "i	only output Idle state"
+		      , &idle,
+
+		      TINO_GETOPT_STRING
+		      "p prfx	set Prefix for variables"
+		      , &prefix,
 
 		      TINO_GETOPT_STRING
 		      TINO_GETOPT_DEFAULT
@@ -159,21 +271,53 @@ main(int argc, char **argv)
   int monfd = ok(udev_monitor_get_fd(mon), "bug getting monitor fd");
 #endif
 
-  tino_io_fd(out, "stdout");
+  queue = nonnull(udev_queue_new(udev), "cannot get udev queue");
+
+  tino_io_fd(out, out==1 ? "stdout" : "fd");
   while ((dev=nonnull(udev_monitor_receive_device(mon), "cannot fetch event data from monitor"))!=0)
     {
-      udev_device_unref(dev);
-      tino_put_dec_l(out, 0, udev_device_get_seqnum(dev));
+#if 0
+      tino_put_dec(out, 0, udev_queue_get_udev_is_active(queue));
       tino_io_put(out, ' ');
-      tino_put_ansi_if(out, udev_device_get_action(dev));
-      tino_io_put(out, ' ');
-      tino_put_ansi_if(out, udev_device_get_subsystem(dev));
-      tino_io_put(out, ' ');
-      tino_put_ansi_if(out, udev_device_get_devtype(dev));
-      tino_io_put(out, ' ');
-      tino_put_ansi_if(out, udev_device_get_devnode(dev));
+#endif
+      if (idle && !udev_queue_get_queue_is_empty(queue))
+	continue;
+      if (action && strcmp(action, udev_device_get_action(dev)))
+        continue;
+
+      first = 1;
+
+      if (check(!idle, "idle"))
+        tino_put_dec(out, 0, udev_queue_get_queue_is_empty(queue));
+      if (check(1, "seq"))
+        tino_put_dec_l(out, 0, udev_device_get_seqnum(dev));
+
+      outs(!action,    "action",  udev_device_get_action);
+      outs(!subsystem, "subsys",  udev_device_get_subsystem);
+      outs(!devtype,   "devtype", udev_device_get_devtype);
+      outs(1,          "devnode", udev_device_get_devnode);
+      outs(0,          "syspath", udev_device_get_syspath);
+      outs(0,          "sysname", udev_device_get_sysname);
+      outs(0,          "sysnum",  udev_device_get_sysnum);
+      outs(0,          "driver",  udev_device_get_driver);
+
+      if (check(0, "devlinks"))
+	lister(udev_device_get_devlinks_list_entry(dev));
+      if (check(0, "tags"))
+	lister(udev_device_get_tags_list_entry(dev));
+
+      if (check(-1, "properties"))
+	lister2(dev, "prop_", udev_device_get_properties_list_entry, udev_device_get_property_value);
+#if 0
+struct udev_list_entry *udev_device_get_sysattr_list_entry(struct udev_device *);
+
+      if (check(-1, "sysattr"))
+	lister2(dev, "sys_", udev_device_get_sysattr_list_entry, udev_device_get_sysattr_value);
+#endif
       tino_io_put(out, '\n');
       tino_io_flush_write(out);
+
+      udev_device_unref(dev);
       dev = 0;
     }
   cleanup();
